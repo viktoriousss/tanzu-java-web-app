f1cad5f0a38aa09525299f2d385f332cdc3993bf
Viktor <viktorvdberg@yahoo.com>
2022-11-25 17:44:15 +0100

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu @ VMUG.SE 15:03";
+		return "Greetings from Spring Boot + Tanzu @ 17:24";
 	}
 
 }
