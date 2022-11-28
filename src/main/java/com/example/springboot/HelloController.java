56a5610900d943c86d1f6d7d74036309c7cc4f44
Viktor <viktorvdberg@yahoo.com>
2022-11-28 10:45:19 +0100

    update during SE meeting in Sweden

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu @ 09:14";
+		return "Greetings from Spring Boot + Tanzu - SE Meeting in Sweden @ 10:42";
 	}
 
 }
